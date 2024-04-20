a0c92afda09a57cc688137f6096086f2f9eae363
raimirarara <raimi.rarara@gmail.com>
2024-04-20 14:24:18 +0900

    use useState, useEffect in Preview

--- components/RenderReactComponent.tsx
+++ components/RenderReactComponent.tsx
@@ -1,46 +1,48 @@
-'use client'
-import React, { useEffect, useRef } from "react";
-import * as Babel from '@babel/standalone';
-import { createRoot } from 'react-dom/client';
+"use client"
+import React, { useEffect, useRef, useState } from "react"
+import * as Babel from "@babel/standalone"
+import { createRoot } from "react-dom/client"
 
-export default function RenderReactComponent({ componentCode }: {
-    componentCode: string;
-}) {
-    const containerRef = useRef<HTMLDivElement>(null);
-    const rootRef = useRef<any>(null);  // createRoot のインスタンスを保持するための ref
+export default function RenderReactComponent({ componentCode }: { componentCode: string }) {
+  const containerRef = useRef<HTMLDivElement>(null)
+  const rootRef = useRef<any>(null) // createRoot のインスタンスを保持するための ref
 
-    useEffect(() => {
-        if (componentCode && containerRef.current) {
-            // containerRef.current が存在し、まだルートが生成されていない場合にのみ createRoot を呼び出す
-            if (!rootRef.current) {
-                rootRef.current = createRoot(containerRef.current);
-                const doc = containerRef.current.ownerDocument;
-                const head = doc.head;
+  useEffect(() => {
+    if (componentCode && containerRef.current) {
+      // containerRef.current が存在し、まだルートが生成されていない場合にのみ createRoot を呼び出す
+      if (!rootRef.current) {
+        rootRef.current = createRoot(containerRef.current)
+        const doc = containerRef.current.ownerDocument
+        const head = doc.head
 
-                // Tailwind CSSをドキュメントの<head>に追加
-                const script = doc.createElement('script');
-                script.src = 'https://cdn.tailwindcss.com';
-                head.appendChild(script);
-            }
-            try {
-                // コードをトランスパイル
-                const transformedCode = Babel.transform(componentCode, {
-                    presets: ['react']
-                }).code;
+        // Tailwind CSSをドキュメントの<head>に追加
+        const script = doc.createElement("script")
+        script.src = "https://cdn.tailwindcss.com"
+        head.appendChild(script)
+      }
+      try {
+        // コードをトランスパイル
+        const transformedCode = Babel.transform(componentCode, {
+          presets: ["react"],
+        }).code
 
-                console.log("Transformed code:", transformedCode);
+        console.log("Transformed code:", transformedCode)
 
-                // トランスパイルされたコードを関数として評価
-                const Component = new Function('React', `return function CustomComponent() { return ${transformedCode}; }`)(React);
-                console.log("Component:", Component);
+        // トランスパイルされたコードを関数として評価
+        const Component = new Function("React", "useState", "useEffect", `return ${transformedCode}`)(
+          React,
+          useState,
+          useEffect
+        )
+        console.log("Component:", Component)
 
-                // 既存のルートを使用してレンダリング
-                rootRef.current.render(<Component />);
-            } catch (error) {
-                console.error("Error rendering component:", error);
-            }
-        }
-    }, [componentCode]);
+        // 既存のルートを使用してレンダリング
+        rootRef.current.render(<Component />)
+      } catch (error) {
+        console.error("Error rendering component:", error)
+      }
+    }
+  }, [componentCode])
 
-    return <div ref={containerRef} />;
-};
+  return <div ref={containerRef} />
+}
